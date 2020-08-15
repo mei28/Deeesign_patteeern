1c5b6ac68f6507d0481898341a2fb0597b4ee0bd
KindMaple <angelattack9759@gmail.com>
2020-08-16 02:08:47 +0900

    ADD: main処理

---
+++ 10-Strategy/main.rb
@@ -0,0 +1,14 @@
+# 以下のコードを参考にした
+# https://github.com/nipe0324/design_patterns_ruby/blob/master/strategy/main.rb
+
+require './report'
+require './html_formatter'
+require './plain_text_formatter'
+
+puts '===== HTML形式の場合 ====='
+report = Report.new(HTMLFormatter.new)
+report.print_output
+
+puts '===== プレーンテキスト形式の場合 ====='
+report.formatter = PlainTextFormatter.new
+report.print_output
