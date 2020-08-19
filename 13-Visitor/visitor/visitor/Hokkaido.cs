395653f654656286f855cc5d8a7bc240a5028903
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-08-20 00:04:53 +0900

    ADD: Site class

---
+++ 13-Visitor/visitor/visitor/Hokkaido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace visitor
+{
+    class Hokkaido : Site
+    {
+        public override void Accept(IVisitor visitor)
+        {
+            Console.WriteLine("************北海道************");
+            Console.WriteLine("");
+
+            visitor.visit(this);
+            Console.WriteLine("ﾏｲ ﾈｰﾑ ｲｽﾞ ｸﾗｰｸ. ﾎﾞｰｲｽﾞ ﾋﾞｰ ｱﾝﾋﾞｬｽ!");
+            Console.WriteLine("ｵﾐﾔｹﾞﾆ ｻｯﾎﾟﾛﾉｳｶﾞｯｺｳｸｯｷｰ ﾄﾞｳｿﾞ");
+            Console.WriteLine("");
+        }
+    }
+}
