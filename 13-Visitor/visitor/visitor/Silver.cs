07c0c3f1e2f991e3d6e0c1fd0a9cfdcad05ea325
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-08-20 00:05:21 +0900

    ADD: Visitor class

---
+++ 13-Visitor/visitor/visitor/Silver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace visitor
+{
+    class Silver : IVisitor
+    {
+        public void visit(Tokyo tokyo)
+        {
+            Console.WriteLine("東京じゃあ～！浅草寺が趣深いのぉ～");
+
+        }
+
+        public void visit(Kyoto kyoto)
+        {
+            Console.WriteLine("京都じゃよ！八つ橋がのどに詰まるんじゃあ…");
+
+        }
+
+        public void visit(Hakata hakata)
+        {
+            Console.WriteLine("博多じゃ。天神様にお参りじゃ～");
+
+        }
+
+        public void visit(Hokkaido hokkaido)
+        {
+            Console.WriteLine("北海道じゃぞ～、年寄りに優しい気温じゃの～");
+
+        }
+    }
+}
