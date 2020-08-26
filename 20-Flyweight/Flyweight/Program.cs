18ea417f6d9e66d78605587303f773c0019513f9
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-08-27 01:50:07 +0900

    ADD: Flyweight pattern

---
+++ 20-Flyweight/Flyweight/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = Stamp.Get('a');
+            var a2 = Stamp.Get('a');
+            var b = Stamp.Get('b');
+
+            Console.WriteLine(a == a2);
+            Console.WriteLine(a == b);
+        }
+    }
+}
---
+++ 20-Flyweight/Flyweight/Stamp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    public class Stamp
+    {
+        char type;
+
+        private Stamp(char type)
+        {
+            this.type = type;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.type);
+        }
+
+        static Dictionary<char, Stamp> cache = new Dictionary<char, Stamp>();
+
+        public static Stamp Get(char type)
+        {
+            if (!cache.ContainsKey(type))
+                cache.Add(type, new Stamp(type));
+            return cache[type];
+        }
+    }
+}
