17ea4e0e4a5ffe6280febe6be6ee850a1f0c2303
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-08-30 14:07:22 +0900

    ADD: MultipleSymbol

---
+++ 23-Interpreter/nanaka/Interpreter/MultipleSymbol.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interpreter
+{
+    public class MultipleSymbol : Symbol
+    {
+        /// <summary>
+        /// 1 * 1 * 2 * 3
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override int Perse(IEnumerable<string> context)
+        {
+            Queue<string> contextQueue = new Queue<string>(context);
+
+            var leftNum = new NumberSymbol();
+            var res = leftNum.Perse(new List<string>() { contextQueue.Dequeue() });
+
+            while (contextQueue.Count > 0)
+            {
+                switch (contextQueue.Dequeue())
+                {
+                    case "*":
+                        var rightNum = new NumberSymbol();
+                        res *= rightNum.Perse(new List<string>() { contextQueue.Dequeue() });
+                        break;
+                    case "/":
+                        var rightNum2 = new NumberSymbol();
+                        res /= rightNum2.Perse(new List<string>() { contextQueue.Dequeue() });
+                        break;
+                    default:
+                        throw new InvalidOperationException("MultipleSymbol");
+                }
+
+            }
+
+            return res;
+        }
+    }
+}
