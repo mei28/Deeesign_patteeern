e91282329de1e1434e89a1c26fbb735c95fc79f0
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-08-30 14:09:54 +0900

    ADD: main

---
+++ 23-Interpreter/nanaka/Interpreter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new AddSymbol();
+            try
+            {
+                Console.WriteLine(test.Perse("2 * 3 + 4 * 5 * 6".Split(" ")));
+                Console.WriteLine(test.Perse("6 / 3 - 4".Split(" ")));
+                Console.WriteLine(test.Perse("6 / 3 ? 2".Split(" ")));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
