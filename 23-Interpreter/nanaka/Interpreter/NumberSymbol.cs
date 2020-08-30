25e0ef0ec487772bc4975fcf2a08a039e03076d6
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-08-30 14:06:59 +0900

    ADD: NumberSymbol class

---
+++ 23-Interpreter/nanaka/Interpreter/NumberSymbol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interpreter
+{
+    public class NumberSymbol : Symbol
+    {
+        public override int Perse(IEnumerable<string> context)
+        {
+            if (context.Count() != 1)
+                throw new InvalidOperationException("NumberSymbol");
+            return Convert.ToInt32(context.First());
+        }
+    }
+}
